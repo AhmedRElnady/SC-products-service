7dd45b42e79b06b3f055114b9807f690271de69a
ahmed elnady <dev.elnady@gmail.com>
2019-04-18 00:53:56 +0200

    initial commit

---
+++ config/default.js
@@ -0,0 +1,12 @@
+module.exports = {
+    MS: {
+        product: {
+           url: "http://localhost:4000",
+           prefix: "products"
+        },
+        shopping_cart: {
+           url: "http://localhost:5000",
+           prefix: "shopping-carts"
+        }
+     }
+}
