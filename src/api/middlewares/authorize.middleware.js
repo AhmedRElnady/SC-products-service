f8981dc4dfcfb186529f56186f11b81123b24b66
ahmed elnady <dev.elnady@gmail.com>
2019-04-19 02:37:22 +0200

    authorize middleware implementaion

--- src/api/middlewares/authorize.middleware.js
+++ src/api/middlewares/authorize.middleware.js
@@ -2,9 +2,31 @@ const config = require('config');
 
 function authorize() {
     return (req, res, next) => {
-        (async()=> {
-            console.log(".... request is authorized...")
-            next()
+        (async () => {
+            // console.log(".... request is authorized...")
+            // next()
+            const tokenPayload = JSON.parse(req.headers['x-payload-header']),
+                userId = tokenPayload.id,
+                userRole = tokenPayload.role,
+                reqResource = req.route.path,
+                reqMethod = req.method.toLowerCase(),
+                roles = config.get('acl.roles');
+
+            for (let role in roles) {
+                if (role === userRole) {
+                    const roleObj = roles[role],
+                        allowedResources = roleObj.resources,
+                        allowedPermissions = roleObj.permissions;
+
+
+                    if (allowedResources.includes(reqResource) && allowedPermissions.includes(reqMethod)) {
+                        next();
+                    } else {
+                        res.json({ msg: "Insufficient permissions to access resource" }) // 403
+                    }
+                    break;
+                }
+            }// end for 
         })()
     }
 }
